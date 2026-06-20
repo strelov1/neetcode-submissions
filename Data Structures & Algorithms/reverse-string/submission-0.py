d55108985764582b3ea73bf8b633f8f5c6fc59c3
Ilya Strelov <strelov1@users.noreply.github.com>
2026-06-20 14:15:32 -0300

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        # ["n","e","e","t"]
+        #        h
+        #            t
+        #  ["t","e","e","n"]
+        h = 0
+        t = len(s)-1
+        while h < t:
+            s[h], s[t] = s[t], s[h]
+            h += 1
+            t -= 1
